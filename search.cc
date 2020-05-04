a7847ba0aaa4107227fd89e12a5db964f913c7d4
Henry Sloan <henryksloan@gmail.com>
2020-05-04 15:30:14 -0400

    Update command line arguments and add testing/algorithm options

--- search.cc
+++ search.cc
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <fstream>
+#include <unordered_map>
 
 #include "rabin_karp.h"
 #include "boyer_moore.h"
+#include "naive.h"
+#include "kmp.h"
+#include "data_generator.h"
 
 /**returns index of argv[2] if it exists
  * if it doesn't exist it returns -1**/
 int go(int argc, char* argv[]){
 
-    if(argc != 3){
+    if(argc < 3){
         return -2;
     }
 
@@ -27,10 +32,33 @@ int go(int argc, char* argv[]){
 
     std::string word {argv[2]};
 
-    // rabinKarp search;
-    boyerMoore search;
-
-    int x = search.search(&content, &word);
+    int x;
+    if (argc > 3) {
+        if (std::strcmp(argv[3], "rabinkarp") == 0) {
+            rabinKarp search;
+            x = search.search(&content, &word);
+        }
+        else if (std::strcmp(argv[3], "boyermoore") == 0) {
+            boyerMoore search;
+            x = search.search(&content, &word);
+        }
+        else if (std::strcmp(argv[3], "naive") == 0) {
+            naive search;
+            x = search.search(&content, &word);
+        }
+        else if (std::strcmp(argv[3], "kmp") == 0) {
+            KMP search;
+            x = search.search(&content, &word);
+        }
+        else {
+            std::cerr << "Unknown algorithm \"" << argv[3] << "\"\n";
+            std::exit(1);
+        }
+    }
+    else {
+        naive search;
+        x = search.search(&content, &word);
+    }
 
     inFile.close(); //CLOSE
 
@@ -40,18 +68,35 @@ int go(int argc, char* argv[]){
 
 
 int main(int argc, char* argv[]){
+    if (argc > 1 && (std::strcmp(argv[1], "-g") == 0)) {
+        dataGenerator gen;
+
+        rabinKarp rabin_karp;
+        gen.generate("rabinkarp", rabin_karp);
+
+        boyerMoore boyer_moore;
+        gen.generate("boyermoore", boyer_moore);
+
+        naive naive;
+        gen.generate("naive", naive);
+
+        KMP kmp;
+        gen.generate("kmp", kmp);
+
+        return 0;
+    }
 
     int retVal = go(argc, argv);
 
     if(retVal == -1){
-      std::cout<< "The word is not in the text file" << argv[1] << std::endl;
+      std::cout<< "The word is not in the text file " << argv[1] << std::endl;
     }
     else if(retVal == -2){
-      std::cerr << "usage: ./search FILE 'word'" << std::endl;
+      std::cerr << "usage: ./search FILE 'word' [naive/kmp/rabinkarp/boyermoore]" << std::endl;
     }
     else{
       std::cout << "The word can be found at character location " << retVal << std::endl;
     }
-    
+
     return 0;
 }
